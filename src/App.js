02d2db7f689442eba62e281413b8db5da505408c
unknown <sapkotabivishan@gmail.com>
2024-09-05 22:03:30 +0545

    extra comment file added

--- src/App.js
+++ src/App.js
@@ -6,21 +6,23 @@ import { useState } from 'react';
 //creating an array through Array() function where spread operator helps to combine each length of array in one line.
 const createArray = (length) => [...Array(length)];
 
-function Star({ selected = false, onSelect }) {
-  return <FaStar color={selected ? 'red' : 'grey'} onClick={onSelect} />
+function Star({ selected = false, onSelect, }) {
+  return <FaStar
+    color={selected ? 'red' : 'grey'}
+    onClick={onSelect}
+  />
 }
 
 function StarRating({ countStart = 5 }) {
-  const [selectedStars, setSelectedStars] = useState(0); // don't select at initial stage when component render.
-  //returning star component by creating given length of array.
+  const [selectedStars, setSelectedStars] = useState(0);
   return createArray(countStart).map((n, i) => (
-    <Star key={i} selected={selectedStars > i} //to keep track of selected stars based on index.
-      onSelect={() => setSelectedStars(i + 1)} /> //selecting stars based on index, where our base index is 0 so +1 and making sure capturing it correctly.
+    <Star key={i} selected={selectedStars > i}
+      onSelect={() => setSelectedStars(i + 1)} />
   ))
 }
 
 function App() {
-  return <StarRating countStart={10} />
+  return <StarRating />
 }
 
 export default App;
---
+++ src/AppWithCmnts.js
@@ -0,0 +1,32 @@
+import logo from './logo.svg';
+import './App.css';
+import { FaStar } from "react-icons/fa";
+import { useState } from 'react';
+
+//creating an array through Array() function where spread operator helps to combine each length of array in one line.
+const createArray = (length) => [...Array(length)];
+
+function Star({ selected = false, onSelect, ey }) {
+  // return <h1>{ey}</h1>
+  return <FaStar
+    //onClick={() => console.log(ey)} 
+    color={selected ? 'red' : 'grey'}
+    onClick={onSelect}
+  />
+}
+
+function StarRating({ countStart = 5 }) {
+  const [selectedStars, setSelectedStars] = useState(0); // not selecting at initial stage when component render.
+  //returning star component by creating given length of array.
+  return createArray(countStart).map((n, i) => (
+    <Star key={i} selected={selectedStars > i} //to keep track of selected stars based on index.
+      onSelect={() => setSelectedStars(i + 1)} /> //selecting stars based on index, where our base index is 0 so +1 and making sure capturing it correctly. Where we should sett selected stars more than 1 or do +1, otherwise it will set it as in initial stage and star rating wouldn't work properly. Because we have set selectedStars state as 0 so even if we put 1 or 2 or 3, we have to do +1 anyways to capture the star index correctly.
+
+  ))
+}
+
+function App() {
+  return <StarRating />
+}
+
+export default App;
