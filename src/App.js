9938c184d2177daecc4a466b9a26dd8d8907fb9b
unknown <sapkotabivishan@gmail.com>
2024-09-05 18:44:11 +0545

    initiation of project rating

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
+import { FaStar } from "react-icons/fa";
+import { useState } from 'react';
+
+//creating an array through Array() function where spread operator helps to combine each length of array in one line.
+const createArray = (length) => [...Array(length)];
+
+function Star({ selected = false, onSelect }) {
+  return <FaStar color={selected ? 'red' : 'grey'} onClick={onSelect} />
+}
+
+function StarRating({ countStart = 5 }) {
+  const [selectedStars, setSelectedStars] = useState(0); // don't select at initial stage when component render.
+  //returning star component by creating given length of array.
+  return createArray(countStart).map((n, i) => (
+    <Star key={i} selected={selectedStars > i} //to keep track of selected stars based on index.
+      onSelect={() => setSelectedStars(i + 1)} /> //selecting stars based on index, where our base index is 0 so +1 and making sure capturing it correctly.
+  ))
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <StarRating countStart={10} />
 }
 
 export default App;
